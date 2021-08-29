006ae2473a076a8bd946afa1fd015dd0a2527afa
Peggy Tsai <pinklover1671291991@gmail.com>
2021-08-29 23:07:26 +0800

    Update 2021-08-29T15:07:24.100Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,12 +4,36 @@ $(function () {
   console.log('Hello Bootstrap5');
 });
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
+  slidesPerView: 'auto',
   spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  grabCursor: true
+});
+var swiper = new Swiper(".SwiperGroup", {
+  slidesPerView: 'auto',
+  grid: {
+    rows: 1,
+    //slidesPerColumn: 2, swiper6寫法
+    fill: 'row' //slidesPerColumnFill: 'row',  swiper6寫法
+
+  },
+  spaceBetween: 10,
+  width: 500,
   breakpoints: {
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 30
+    767: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 30,
+      grid: {
+        rows: 2,
+        //slidesPerColumn: 2, swiper6寫法
+        fill: 'row' //slidesPerColumnFill: 'row',  swiper6寫法
+
+      },
+      width: 800
     }
   }
 });
