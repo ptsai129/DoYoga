af325075c604fb8f5283ee6fe1a0eb7bb7fe65e1
Peggy Tsai <pinklover1671291991@gmail.com>
2021-08-31 17:08:40 +0800

    Update 2021-08-31T09:08:33.866Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -12,29 +12,10 @@ var swiper = new Swiper(".mySwiper", {
   },
   grabCursor: true
 });
-var swiper = new Swiper(".SwiperGroup", {
-  slidesPerView: 'auto',
-  grid: {
-    rows: 1,
-    //slidesPerColumn: 2, swiper6寫法
-    fill: 'row' //slidesPerColumnFill: 'row',  swiper6寫法
-
-  },
-  spaceBetween: 10,
-  width: 500,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      slidesPerGroup: 1,
-      spaceBetween: 30,
-      grid: {
-        rows: 2,
-        //slidesPerColumn: 2, swiper6寫法
-        fill: 'row' //slidesPerColumnFill: 'row',  swiper6寫法
-
-      },
-      width: 800
-    }
-  }
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {
+  nextArrow: '>',
+  prevArrow: '<',
+  buttonClass: 'btn primary'
 });
 //# sourceMappingURL=all.js.map
